d8a22ae725eebd5651c8455e7ac3aad1f75f14ea
John Doe <noluthandombali4@gmail.com>
2022-07-07 03:17:29 +0200

    all file commited

---
+++ pizza-cart-api.js
@@ -0,0 +1,19 @@
+document.addEventListener('alpine:init', () => {
+
+    Alpine.data('pizzaCartWithAPIWidget', function () {
+        return {
+            init() {
+
+
+                axios
+                    .get('https://pizza-cart-api.herokuapp.com/api/pizzas')
+                    .then((result) => {
+                        this.pizzas = result.data.pizzas
+                    })
+            },
+            message: 'Eating pizzas',
+
+            pizzas: []
+        }
+    });
+})
